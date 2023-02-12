53c28f6d0066bce02ee87f3f2342e4f7fe90124d
ThinhK20 <74862129+ThinhK20@users.noreply.github.com>
2023-02-12 19:39:29 +0700

    Optimize auth

--- backend/routes/audioRouter.js
+++ backend/routes/audioRouter.js
@@ -1,15 +1,14 @@
-const express = require('express');
-const AudioController = require('../controllers/audioController');
-const uploadMulter = require('../utils');
+const express = require("express");
+const AudioController = require("../controllers/audioController");
+const uploadMulter = require("../utils");
 
-const router = express.Router() 
+const router = express.Router();
 
+router.post("/upload", uploadMulter.single("audio"), AudioController.upload);
+router.get("/topic/:topic", AudioController.getAudiosFromTopic);
+router.get("/", AudioController.getAllAudio);
+router.get("/songs", AudioController.getAllSongWithoutNoise);
 
-router.post('/upload', uploadMulter.single('audio') ,AudioController.upload) 
-router.get('/topic/:topic', AudioController.getAudiosFromTopic) 
-router.get('/', AudioController.getAllAudio) 
-router.get('/songs', AudioController.getAllSongWithoutNoise) 
+router.get("/:audioName", AudioController.renderAudio);
 
-router.get('/:audioName', AudioController.renderAudio)
-
-module.exports = router
+module.exports = router;
