b5723b0dc983d2e944dd36096d0d35dfe148172e
ThinhK20 <74862129+ThinhK20@users.noreply.github.com>
2023-03-18 22:27:48 +0700

    Fix cors privacy(12)

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,19 @@ const errorHandlerMiddleware = require("./middlewares/ErrorHandlerMiddleware");
 dotenv.config();
 const app = express();
 
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, DELETE, OPTIONS"
+   );
+   next();
+});
+
 app.use(
    cors({
       allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
